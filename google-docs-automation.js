7cc71a97f95b66ad0dd231d2b061e2c1f128fd53
jonschaapkoe <80953906+jonschaapkoe@users.noreply.github.com>
2023-05-10 17:30:37 +0200

    Create google-docs-automation.js

---
+++ google-docs-automation.js
@@ -0,0 +1,18 @@
+// Press Ctrl + Shift + C
+document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': 67, 'ctrlKey': true, 'shiftKey': true}));
+document.dispatchEvent(new KeyboardEvent('keyup', {'keyCode': 67, 'ctrlKey': true, 'shiftKey': true}));
+
+// Wait for the label to load
+var labelInterval = setInterval(function() {
+    var label = document.getElementById('goog_1409111978.label');
+    if (label) {
+        clearInterval(labelInterval);
+
+        // Click on the label
+        label.click();
+
+        // Press Esc
+        document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': 27}));
+        document.dispatchEvent(new KeyboardEvent('keyup', {'keyCode': 27}));
+    }
+}, 100);
